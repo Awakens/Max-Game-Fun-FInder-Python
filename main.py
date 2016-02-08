05fb0a3910348ac36cf2ed1b2bb1d454d10e1922
Awakens <akaxd123@gmail.com>
2016-02-08 15:37:06 -0500

    Fix token removal and refill

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ fun = [4,1,2,3]
 max_fun = 0
 max_fun_list = []
 current_list = []
-TOKEN_CAPACITY = 150
-tokens = TOKEN_CAPACITY
+tokens = cap
 current_games_played = []
 current_fun = 0
 
@@ -14,13 +13,18 @@ def total_fun(current_fun, tokens):
     global max_fun, max_fun_list
     for x in fun:
         if tokens > 0:
+            prev_tokens = tokens
+            tokens -= 1
+            if current_list:                    #don't want to refill if first game, check if list is empty
+                try:
+                    if(current_list.index(x) == -1):  #refill tokens if you play a new game? TODO
+                        pass
+                except:                               #is a new game
+                    tokens += refill
+                    if tokens > cap:
+                        tokens = cap
             current_list.append(x)
             current_fun += x
-            tokens -= 1
-            if(current_list[-1] != x):  #refill tokens if you play a different game? or new one? TODO
-                tokens += refill
-                if tokens > TOKEN_CAPACITY:
-                    tokens = TOKEN_CAPACITY
             if current_fun > max_fun:
                 played_all_games = True;
                 for games in fun:                 #test all games have been played
@@ -37,7 +41,9 @@ def total_fun(current_fun, tokens):
             total_fun(current_fun, tokens)
             current_list.pop()
             current_fun -= x
+            tokens = prev_tokens
 
 
 total_fun(current_fun, tokens)
 print(max_fun_list)   #TODO, incorrect
+print(max_fun)
