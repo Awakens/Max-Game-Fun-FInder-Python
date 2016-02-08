fc44b71372e6d07085a31429eced1a51909a2b67
Awakens <akaxd123@gmail.com>
2016-02-08 15:59:42 -0500

    Fix tokens are refilled between new games
    Reposition refill to outside of tokens >0 check

--- main.py
+++ main.py
@@ -2,27 +2,26 @@ num = 4
 cap = 5
 refill = 2
 fun = [4,1,2,3]
+tokens = cap
 max_fun = 0
 max_fun_list = []
 current_list = []
-tokens = cap
-current_games_played = []
 current_fun = 0
 
 def total_fun(current_fun, tokens):
     global max_fun, max_fun_list
     for x in fun:
+        prev_tokens = tokens
+        if current_list:                    #don't want to refill if first game, check if list is empty
+            try:
+                if(current_list.index(x) != -1):  #refill tokens if you play a new game TODO
+                    pass
+            except:                               #is a new game
+                tokens += refill
+                if tokens > cap:
+                    tokens = cap
         if tokens > 0:
-            prev_tokens = tokens
             tokens -= 1
-            if current_list:                    #don't want to refill if first game, check if list is empty
-                try:
-                    if(current_list.index(x) == -1):  #refill tokens if you play a new game? TODO
-                        pass
-                except:                               #is a new game
-                    tokens += refill
-                    if tokens > cap:
-                        tokens = cap
             current_list.append(x)
             current_fun += x
             if current_fun > max_fun:
